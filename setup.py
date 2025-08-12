cfcbce8f13fe18d40e596fca6915c23024026c1b
Sonalikasingh17 <singhsonalika5@gmail.com>
2025-08-12 12:56:14 +0530

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+HYPEN_E_Dot = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    """
+    This function reads the requirements from a file and returns them as a list.
+    """
+    requirements = []
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements= [req.replace('\n', '') for req in requirements]
+
+    # Remove any hyphen-e dot requirement if it exists
+        if HYPEN_E_Dot in requirements:
+            requirements.remove(HYPEN_E_Dot)
+    return [req.strip() for req in requirements if req.strip()]
+    # return requirements
+
+setup(
+    name='ml_projects',
+    version='0.1',
+    author='Sonalika Singh',
+    author_email='singhsonalika5@gmail.com',
+    description='A collection of machine learning projects',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
